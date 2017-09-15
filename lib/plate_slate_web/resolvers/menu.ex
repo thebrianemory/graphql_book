43258368fc3f06671cc1c13e520e959d038d85af
thebrianemory <the@brianemory.com>
2017-09-15 09:27:43 -0700

    Update to current point in book

--- lib/plate_slate_web/resolvers/menu.ex
+++ lib/plate_slate_web/resolvers/menu.ex
@@ -12,4 +12,15 @@ defmodule PlateSlateWeb.Resolvers.Menu do
   def menu_items(_, args, _) do
     {:ok, Menu.list_items(args)}
   end
+
+  def search(_, %{matching: term}, _) do
+    {:ok, Menu.search(term)}
+  end
+
+  def items_for_category(category, _, _) do
+    query = Ecto.assoc(category, :items)
+    {:ok, PlateSlate.Repo.all(query)}
+  end
+
+
 end
--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -14,11 +14,18 @@ defmodule PlateSlateWeb.Schema do
   import_types __MODULE__.MenuTypes
 
   query do
+    # Other query fields
+
 
     field :menu_items, list_of(:menu_item) do
       arg :filter, :menu_item_filter
       arg :order, type: :sort_order, default_value: :asc
       resolve &Resolvers.Menu.menu_items/3
+    end
+
+    field :search, list_of(:search_result) do
+      arg :matching, non_null(:string)
+      resolve &Resolvers.Menu.search/3
     end
 
   end
@@ -44,37 +51,4 @@ defmodule PlateSlateWeb.Schema do
     value :desc
   end
 
-  @desc "Filtering options for the menu item list"
-  input_object :menu_item_filter do
-
-    @desc "Matching a name"
-    field :name, :string
-
-    @desc "Matching a category name"
-    field :category, :string
-
-    @desc "Matching a tag"
-    field :tag, :string
-
-    @desc "Priced above a value"
-    field :priced_above, :float
-
-    @desc "Priced below a value"
-    field :priced_below, :float
-
-    @desc "Added to the menu before this date"
-    field :added_before, :date
-
-    @desc "Added to the menu after this date"
-    field :added_after, :date
-
-  end
-
-  object :menu_item do
-    field :id, :id
-    field :name, :string
-    field :description, :string
-    field :added_on, :date
-  end
-  # Common types; :date, :sort_order, etc
 end
